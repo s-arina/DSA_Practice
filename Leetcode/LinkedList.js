1527b5cb9df524f7bd3f9e7c4299b22d0c43273c
Sarina Chang <sarinachang7@gmail.com>
2022-08-29 17:12:33 -0400

    linked list nightmares

--- Leetcode/LinkedList.js
+++ Leetcode/LinkedList.js
@@ -103,4 +103,68 @@ console.log(middleNode([1, 2, 3, 4, 5, 6])); // [4, 5, 6]
 */
 
 // =====
-// TIME – O(), SPACE – O()
+// TIME – O(n), SPACE – O(1)
+
+function reverseList(head) {
+  // we want the new head to be pointing to null after its reverse
+  let prev = null;
+  // track the current head
+  let current = head;
+
+  // while the current node does not point to null
+  while (current !== null) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+}
+
+/*
+input
+    1 -> 2 -> 3 -> 4 -> 5 -> null
+
+    notes:
+    we need to keep track of previous, current, and next
+
+    EXAMPLE:
+    1) current = head --> 3, prev --> 2
+
+    1 <- 2 -> 3 -> 4 -> 5 -> null
+         p    c    
+
+    2) next = current.next --> 4
+      to keep the link from 3 -> 4 when it gets severed
+
+    1 <- 2 -> 3 -> 4 -> 5 -> null
+         p    c    n
+
+    3) current.next = prev --> 2 store the previous value 
+      this will erase the link from 3 -> 4 and set a link from to 2 <- 3, reversing it
+
+    1 <- 2 <- 3    4 -> 5 -> null
+         p    c    n
+        c.n
+
+    4) prev = current --> 3
+      previous has to move to current now 
+
+    1 <- 2 <- 3    4 -> 5 -> null
+              c    n
+         c.n  p
+
+    5) current = next --> 4
+      since the next node was saved on top, we can now move current (3) to the next node (4)
+
+    1 <- 2 <- 3    4 -> 5 -> null
+              p    n
+         c.n       c
+
+    6) return prev
+      the new head
+
+output
+    null <- 1 <- 2 <- 3 <- 4 <- 5
+*/
