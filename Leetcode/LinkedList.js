b8600a62853c179e5de6458904a3865b0807717b
Sarina Chang <sarinachang7@gmail.com>
2022-09-02 05:32:13 -0400

    remove linked list ele

--- Leetcode/LinkedList.js
+++ Leetcode/LinkedList.js
@@ -3,9 +3,9 @@
 /*
     - Linked List Cycle
     - Middle of Linked List
-    to do:
     - Palindrome Linked List
     - Reversed Linked List
+    to do:
     - Remove Linked List Elements
     - Merged Two Linked Lists
     - Remove Duplicates from Sorted List
@@ -92,6 +92,9 @@ console.log(middleNode([1, 2, 3, 4, 5, 6])); // [4, 5, 6]
     - return true if the list is a palindrome, else false
 */
 
+// ===== HELPER FUNCTIONS (TWO POINTERS MOSTLY)
+// TIME – O(n), SPACE – O(1)
+
 /*
 THE IDEA:
   - get the middle node of the list
@@ -100,7 +103,7 @@ THE IDEA:
   - if all the nodes match, it's a palindrome list
 
 NOTES:
-- 3 helper functions (middle of list, reverse list, compare lists)
+  - 3 helper functions (middle of list, reverse list, compare lists)
 
 EXAMPLE:
 
@@ -115,14 +118,13 @@ input
 
   1 -> 2 -> 3
 
-  3) compare it to the initial list
+  3) compare the values of the reversed list to the values of the initial list
 
   1 -> 2 -> 3 -> 2 -> 1
 
   1 -> 2 -> 3
 
   MATCH === PALINDROME
-
 */
 
 // find the middle node of a list
@@ -175,15 +177,11 @@ function palindromeList(head) {
   // if they're the same, it's a palindrome list
 }
 
-// =====
-// TIME – O(), SPACE – O()
-
 // ============================== REVERSED LINKED LIST ==============================
 // ============================== REVERSED LINKED LIST ==============================
 
-/* Given the head of a linked list and an integer val:
-    - remove all the nodes of the linked list that has Node.val == val
-    - return the new head
+/* Given the head of a singly linked list:
+    - reverse the list and return it
 */
 
 // =====
@@ -197,8 +195,8 @@ function reverseList(head) {
   // 1 -> 2 -> 3 -> 4 -> 5
   // c
 
-  // while the current node does not point to null
-  while (current !== null) {
+  // while the current node exists
+  while (current) {
     next = current.next;
     // 1 -> 2 -> 3 -> 4 -> 5
     // c    n
@@ -268,4 +266,102 @@ input
 
 output
     null <- 1 <- 2 <- 3 <- 4 <- 5
+*/
+
+// ============================== REMOVE LINKED LIST ELEMENTS ==============================
+// ============================== REMOVE LINKED LIST ELEMENTS ==============================
+
+/* Given the head of a linked list and an integer val:
+    - remove all the nodes of the linked list that has Node.val == val
+    - return the new head
+*/
+
+// ===== TWO POINTERS (AND DUMMY NODE)
+// TIME – O(n), SPACE – O(1)
+
+function removeElement(head, val) {
+  // dummy node can be any value
+  let dummy = new ListNode(-1);
+  // its next is the current head
+  dummy.next = head;
+  // set prev to be the prev node (dummy, usually null!)
+  let prev = dummy;
+  let curr = head;
+
+  // while the current node exists
+  while (curr) {
+    // keep track of the node after the current one
+    next = curr.next;
+    // if the node value matches the value passed in
+    if (curr.val === val) {
+      // point prev.next to curr.next to sever the link from the current node
+      prev.next = next;
+      // move onto the next node
+      curr = curr.next;
+      // else if the node value does NOT match the value passed in
+    } else {
+      // no need to sever link, move pointers along
+      prev = curr;
+      curr = curr.next;
+    }
+  }
+  // if the first/a node was removed, the dummy will have already been pointing to the new head
+  return dummy.next;
+}
+
+/*
+input
+    1 -> 2 -> 6 -> 3
+
+    NOTES:
+    - we need to keep track of previous, current, and next
+    - EDGE CASE: the first node is the value to be removed
+    - prev has to be an actual node instead of null
+  
+    EXAMPLE:
+    - REMOVE: 1
+
+    1) let dummy = new ListNode(-1);
+       dummy.next = head;
+    - create a dummy node before the current head node
+
+    dummy(-1) -> 1 -> 2 -> 6 -> 3
+       
+    2) let prev = dummy;
+       let curr = head;
+    - set prev to be the dummy (usually null)
+    - set curr to be the current head to track
+
+    3) next = curr.next;
+    - keep track of the next node
+    
+    dummy(-1) -> 1 -> 2 -> 6 -> 3
+        p        c    n
+
+    4a) if (curr.val === val) {
+        prev.next = next;
+        curr = curr.next;
+    }
+    - if the current node val matches the val passed in
+    - point prev.next to curr.next to sever the link from the current node
+    - then move onto the next node
+
+    dummy(-1)    1    2 -> 6 -> 3
+        p             c    n
+                     p.n
+
+    5) return dummy.next;
+    - when current reaches null and all values were removed, return the list with dummy.next
+    - as it was set from the beginning to always point to the head
+
+         -------------
+        |             |
+        |             v
+    dummy(-1)         2 -> 6 -> 3 -> null
+        p                       c    n
+                    
+    4b) else (if curr.val !== val)
+        prev = curr;
+        curr = curr.next;
+    - shift the two pointers ahead and continue checking through the list
 */
