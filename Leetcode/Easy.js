6afbf529af6c44684121ac89b241f86ba57eafc9
Sarina Chang <sarinachang7@gmail.com>
2022-09-18 17:07:26 -0400

    binary search notes / next greatest letter

--- Leetcode/Easy.js
+++ Leetcode/Easy.js
@@ -9,6 +9,7 @@
     - Maximum Subarray
     - Counting Bits
     - Range Sum Query - Immutable
+    - Find Smallest Letter Greater Than Target
 */
 
 // ============================== CONTAINS DUPLICATE ==============================
@@ -227,7 +228,7 @@ Given an integer array nums, handle multiple queries of the following type:
     - calculate the sum of the elements of nums between indices left and right (inclusive)
 
 Implement the NumArray class:
-    - initialized the object with the interger array nums
+    - initializes the object with the integer array nums
     - returns the sum of the elements of nums between indices left and right (inclusive)
 
 */
@@ -284,3 +285,75 @@ NumArray.prototype.sumRange = function (left, right) {
 let arr = new NumArray([5, 6, 7, 8]);
 console.log(arr.sumRange(0, 2)); // 18
 console.log(arr.sumRange(0, 3)); // 26
+
+// ============================== RANGE SUM QUERY - IMMUTABLE ==============================
+// ============================== RANGE SUM QUERY - IMMUTABLE ==============================
+
+/* 
+Given a characters array letters that is sorted in non-decreasing order and a character target:
+    - return the smallest character in the array that is larger than target
+
+Note that the letters wrap around.
+    Ex: 
+    letters = ['a', 'b']
+    target = 'z'
+    output = 'a'
+
+    letters = ['c', 'f', 'j']
+    target = 'a'
+    output = 'c'
+
+** NOTE: WHEN GIVEN A SORTED LIST, MOST OF THE TIME IT WILL BE A BINARY SEARCH PROBLEM
+
+*/
+
+// ===== BINARY SEARCH, TWO POINTERS
+// TIME – O(log(n)), SPACE – O(n)
+
+function nextGreatestLetter(letters, target) {
+  // ['a', 'b'], 'z'
+
+  // conditional check when there's only 2 letters in the array
+  // if a > z OR z >= b, return a
+
+  if (letters[0] > target || target >= letters[letters.length - 1])
+    return letters[0];
+
+  // letters = ['c', 'f', 'j'], 'c'
+
+  let start = 0; // 0
+  let end = letters.length - 1; // 2
+
+  // 1st loop: 0 < 2
+  // 2nd loop: 0 < 1
+  // 3rd loop: 1 < 1, break loop
+  while (start < end) {
+    // 1st loop: mid = 0 + 2 / 2 = 1, floor = 1
+    // 2nd loop: mid = 0 + 1 / 2 = .5, floor = 0
+    let mid = Math.floor((start + end) / 2);
+    // 1st loop: letters[1] ('f') <= target ('c') ? no
+    // 2nd loop; letters [0] ('c') <= target ('c') ? yes
+    if (letters[mid] <= target) {
+      // 2nd loop: 0 = 0 + 1, 1
+      start = mid + 1;
+    } else {
+      // 1st loop: 2 = 1
+      end = mid;
+    }
+  }
+
+  // loop done: return letters[1] => f
+  return letters[start];
+}
+
+console.log(nextGreatestLetter(['c', 'f', 'j'], 'c')); // 'f'
+console.log(nextGreatestLetter(['a', 'b'], 'z')); // 'a'
+
+// ===== BUILT IN - FIND
+// TIME – O(n), SPACE – O(1)
+
+function nextGreatestLetterFind(letters, target) {
+  return letters.find((c) => c > target) || letters[0];
+}
+console.log(nextGreatestLetterFind(['c', 'f', 'j'], 'c')); // 'f'
+console.log(nextGreatestLetterFind(['a', 'b'], 'z')); // 'a'
--- SearchingAlgorithms/BinarySearch.js
+++ SearchingAlgorithms/BinarySearch.js
@@ -13,7 +13,7 @@ It uses the divide and conquer method.
 // ==================== TIME COMPLEXITY
 
 /*
-- O(log n) – worst/average case (but still just as good as O(1))
+- O(log(n)) – worst/average case (but still just as good as O(1))
 - O(1) – best case if the element is the first thing we search for
 */
 
@@ -33,6 +33,7 @@ function binarySearch1(arr, val) {
   while (start <= end) {
     // find the middle element of the array each iteration
     let middle = Math.floor((start + end) / 2);
+    console.log(middle);
     if (arr[middle] === val) {
       // return the middle if its the value passed
       return middle;
@@ -47,7 +48,7 @@ function binarySearch1(arr, val) {
   return -1;
 }
 
-console.log(binarySearch1([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(binarySearch1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7)); // 6
 
 // ==================== ALT VERSION
 
@@ -72,4 +73,101 @@ function binarySearch2(arr, val) {
   }
 }
 
-console.log(binarySearch2([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(binarySearch2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7)); // 6
+
+/*
+input 
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  target = 7 (return the index position)
+
+  1) let start = 0, let end = arr.length - 1
+    - two pointers, beginning and end of the array
+
+    - start = 0
+    - end = 9
+
+  ===
+
+  2) while (start <= end)
+    - while the two pointers haven't met
+  
+  ===
+
+  3) let middle = Math.floor((start + end) / 2)
+    - store the middle index of each iteration
+
+    - first middle = 0 + 9 = 9 / 2 = 4.5 = 4
+    - arr[4] = 5
+
+    1   2   3   4   5   6   7   8   9   10
+  start            mid                  end
+
+  ===
+
+  4) if (arr[middle] = val) return middle
+
+  ===
+
+  5) else if (arr[middle] < val) 
+    - arr[4] (5) < 7
+    - 4 < 7
+
+  start = middle + 1
+  0 = 4 + 1, 5
+
+  - start the loop again
+  - get the middle again using index position values (5 + 9 = 14 / 2 = 7)
+
+  start = 5, mid = 7, end = 9 (index positions)
+
+    1   2   3   4   5   6   7   8   9   10
+                      start    mid      end
+
+  ===
+
+  6) else (arr[middle] > val)
+    - arr[7] (8) > 7
+    - 7 > 7
+
+  end = middle - 1
+  9 = 7 - 1, 6
+
+  - start the loop again
+  - get the middle again using index position values (5 + 6 = 11 / 2 = 5.5 = 5)
+
+  start = 5, mid = 5, end = 6 (index positions)
+
+    1   2   3   4   5   6   7   8   9   10
+                      start end
+                       mid
+
+  ===
+
+  7) else if (arr[middle] < val) 
+    - arr[5] (6) < 7
+    - 5 < 7
+
+  start = middle + 1
+  5 = 5 + 1, 6
+
+  - start the loop again
+  - get the middle again
+
+  start = 6, mid = 6, end = 6 (index positions)
+
+    1   2   3   4   5   6   7   8   9   10
+                          start
+                           end
+                           mid
+
+  ===
+
+  8) if (arr[middle] = val) return middle
+
+  arr[6] = 7
+
+  7 = 7 (target)
+
+  return middle, 6 (index position)
+
+*/
